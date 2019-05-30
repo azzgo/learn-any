9dafca1c665dee5c53ded3362ff0a5bcf353a4d7
Yishan Wen <wittywys@gmail.com>
2019-05-30 10:55:38 +0800

    增加删除相关测试

--- Vue-learn/test/obserable.test.js
+++ Vue-learn/test/obserable.test.js
@@ -27,7 +27,7 @@ test('测试 对象 属性更新', () => {
   testObj.b.c++;
 });
 
-test('测试 数组 更新，所有数组项 都会收到更新', () => {
+test('测试 数组 新增，所有数组项 都会收到更新', () => {
   new Watcher(testObj, 'g', function(newVal) {
     assert.strictEqual(newVal.length, 5);
   });
@@ -38,6 +38,19 @@ test('测试 数组 更新，所有数组项 都会收到更新', () => {
   });
 
   testObj.g.push('item');
+});
+
+test('测试 数组 删除，所有数组项 都会收到更新', () => {
+  new Watcher(testObj, 'g', function(newVal) {
+    assert.strictEqual(newVal.length, 3);
+  });
+
+  new Watcher(testObj, 'g.1', function(newVal) {
+    expect(newVal).toBeDefined();
+    expect(newVal).toBe(3);
+  });
+
+  testObj.g.splice(0, 1)
 });
 
 test('新增 对象 的属性更新', () => {
