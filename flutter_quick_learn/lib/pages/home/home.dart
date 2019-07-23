94733e197fca675d446bd93d8b707f0ac2df2902
Yishan Wen <wittywys@gmail.com>
2019-07-23 16:42:58 +0800

    更改目录结构

---
+++ flutter_quick_learn/lib/pages/home/home.dart
@@ -0,0 +1,35 @@
+
+import 'package:flutter/material.dart';
+import 'stickyItem.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ListView(
+        children: <Widget>[
+          StickyItem()
+        ]
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.brown,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
