23a64a9bbb332e630b0097505625678c827f1996
Yishan Wen <wittywys@gmail.com>
2020-01-20 18:59:16 +0800

    java: 添加 mariadb 依赖

--- go/real-world-api/main.go
+++ go/real-world-api/main.go
@@ -4,10 +4,14 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	api := r.Group("/api")
+
+	// health check
+	api.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
