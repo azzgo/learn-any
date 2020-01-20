0f265a27141f7e774878b73565c4b37772fad23d
Yishan Wen <wittywys@gmail.com>
2020-01-20 16:00:19 +0800

    init java real world api

---
+++ java/real-world-api/src/main/java/net/azzgo/realworldapi/RealWorldApiApplication.java
@@ -0,0 +1,13 @@
+package net.azzgo.realworldapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealWorldApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealWorldApiApplication.class, args);
+	}
+
+}
---
+++ java/real-world-api/src/test/java/net/azzgo/realworldapi/RealWorldApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.azzgo.realworldapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealWorldApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
