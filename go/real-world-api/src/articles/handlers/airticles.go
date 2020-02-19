a8e11217a41714565afa4788b0b8fa474482a4d9
Yishan Wen <wittywys@gmail.com>
2020-02-19 17:04:52 +0800

    work with query default value

--- go/real-world-api/src/articles/handlers/airticles.go
+++ go/real-world-api/src/articles/handlers/airticles.go
@@ -6,7 +6,18 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type arictlesQuery struct {
+	Tag string `form:"tag"`
+	Author string `form:"Author"`
+	Favorited string `form:"favorited"`
+	Limit uint `form:"limit,default=20"`
+	Offset uint `form:"offset"`
+}
+
 // GetArictles godoc
 func GetArictles(c *gin.Context)  {
+	var query arictlesQuery
+	c.ShouldBindQuery(&query)
+
 	c.JSON(http.StatusOK, gin.H {"status": "in building"})
 }
