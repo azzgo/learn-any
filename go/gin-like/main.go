2f626db1bc01164133e4f8f81d5ad1dc5c606606
Yishan Wen <wittywys@gmail.com>
2020-05-21 20:13:25 +0800

    feat: gin-1

--- go/gin-like/main.go
+++ go/gin-like/main.go
@@ -7,6 +7,7 @@ import (
 )
 
 func main() {
+  http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/hello", helloHanlder)
 	log.Fatal(http.ListenAndServe(":9999", nil))
 }
@@ -15,4 +16,8 @@ func helloHanlder(w http.ResponseWriter, req *http.Request) {
 	for k, v := range req.Header {
 		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
 	}
+}
+
+func indexHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprint(w, "URL.Path = %q\n", req.URL.Path)
 }
