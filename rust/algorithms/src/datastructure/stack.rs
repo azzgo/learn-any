ae47dde4ab362f4b30e05103f7ed5d1defe894f7
Yishan Wen <wittywys@gmail.com>
2020-07-08 21:17:05 +0800

    test: test case for match brackets

--- rust/algorithms/src/datastructure/stack.rs
+++ rust/algorithms/src/datastructure/stack.rs
@@ -2,18 +2,22 @@ struct VecStack<T> {
     items: Vec<T>,
 }
 
-
 impl<T> VecStack<T> {
     pub fn pop(&mut self) -> Result<T, &str> {
         let item = self.items.pop();
         match item {
             Some(item) => Ok(item),
-            None => Err("Empty stack can not pop!")
+            None => Err("Empty stack can not pop!"),
         }
-
     }
 
-    
+    pub fn peek(&self) -> Result<&T, &str> {
+        let item = self.items.last();
+        match item {
+            Some(item) => Ok(item),
+            None => Err("Empty stack can not peek!"),
+        }
+    }
     pub fn push(&mut self, item: T) {
         self.items.push(item);
     }
@@ -28,8 +32,8 @@ impl<T> VecStack<T> {
 }
 
 impl<T> VecStack<T> {
-    pub fn new() -> VecStack<T>{
-        return VecStack { items: Vec::new() }
+    pub fn new() -> VecStack<T> {
+        return VecStack { items: Vec::new() };
     }
 }
 
@@ -49,5 +53,35 @@ mod tests {
         if let Ok(n) = num {
             assert_eq!(n, 6);
         }
+    }
+
+    fn check_parenthese(bracket_str: &str) -> bool {
+        let mut match_bracket_stack = VecStack::new();
+        let chars: Vec<&str> = bracket_str.split("").collect();
+        for ch in chars {
+            if ["(", "[", "{"].contains(&ch) {
+                match_bracket_stack.push(ch);
+            }
+
+            if [")", "]", "}"].contains(&ch) {
+                if match_bracket_stack.len() == 0 {
+                    return false;
+                }
+                let item = match_bracket_stack.peek();
+                if let Ok(ch1) = item {
+                    if ch1 == &"(" && ch == ")" || ch1 == &"{" && ch == "}" || ch1 ==&"[" && ch == "]" {
+                        let _ = match_bracket_stack.pop();
+                    }
+                }
+            }
+        }
+
+        return match_bracket_stack.len() == 0;
+    }
+
+    #[test]
+    fn it_used_to_match_brackets() {
+        assert!(check_parenthese("[()]{}{[()()]()}"));
+        assert!(!check_parenthese("[(])"));
     }
 }
