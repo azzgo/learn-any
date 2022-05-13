0520c7c7ef16bfeb19f460b863f9085dc4fcc97b
Ison <azzgo@users.noreply.github.com>
2022-05-13 17:15:53 +0800

    feat: implement simplest tokenize mechinsm

--- compilzion/src/02.ts
+++ compilzion/src/02.ts
@@ -14,18 +14,85 @@ export enum TokenType {
 }
 
 export class Token {
-  constructor(private type: TokenType, private value: string) {}
+  constructor(private type: TokenType, private text: string) {}
 
   getType() {
     return this.type;
   }
 
   getValue() {
-    return this.value;
+    return this.text;
+  }
+
+  toString() {
+    return `${this.type}:    ${this.text}`;
   }
 }
 
 export function tokenize(text: string): Token[] {
-  return []
-}
+  const chars = text.split("");
+  let state = DfaState.Initial as DfaState;
+  let tokenText = "";
+  let tokenType: TokenType | null = null;
+  const tokens: Token[] = [];
 
+  const isAlpah = (char: string) => char.match(/^[A-z]$/);
+  const isDigital = (char: string) => char.match(/^[0-9]$/);
+
+  function initToken(char: string): DfaState {
+    if (tokenText.length > 0 && tokenType !== null) {
+      tokens.push(new Token(tokenType, tokenText));
+      tokenText = "";
+    }
+
+    let newState = DfaState.Initial;
+
+    if (isAlpah(char)) {
+      newState = DfaState.Id;
+      tokenType = TokenType.Identifier;
+      tokenText += char;
+    } else if (isDigital(char)) {
+      newState = DfaState.IntLiteral;
+      tokenType = TokenType.IntLiteral;
+      tokenText += char;
+    } else if (char === ">") {
+      newState = DfaState.GT;
+      tokenType = TokenType.GT;
+      tokenText += char;
+    }
+    return newState;
+  }
+
+  for (const char of chars) {
+    switch (state) {
+      case DfaState.Initial:
+        state = initToken(char);
+        break;
+      case DfaState.Id:
+        if (isDigital(char) || isAlpah(char)) {
+          tokenText += char;
+        } else {
+          state = initToken(char);
+        }
+        break;
+      case DfaState.IntLiteral:
+        if (isDigital(char)) {
+          tokenText += char;
+        } else {
+          state = initToken(char);
+        }
+        break;
+      case DfaState.GT:
+        state = initToken(char);
+        break;
+      default:
+        state = DfaState.Initial;
+    }
+  }
+
+  // push token of last one  and reset to Initial state
+  if (tokenText.length > 0) {
+    initToken('');
+  }
+  return tokens;
+}
